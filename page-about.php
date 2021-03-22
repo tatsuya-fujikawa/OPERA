df63ae4879dfae073327337f679177dd3024a418
tatsuya-fujikawa <fujikawa@j-cool.fm>
2021-03-22 14:36:00 +0900

    WP aboutページ作成

---
+++ page-about.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Template Name: OPERAとは
+*/
+?>
+<?php get_header(); ?>
+    <main>
+        <section class="l-spacer">
+            <div class="c-subtitle">
+                <h2 class="c-title02 u-centerposition" title="About"><span class="u-uppercase">opera</span>とは</h2>
+            </div>
+        </section>
+        <section class="l-about p-about">
+            <div class="l-container -forSub">
+                <div class="p-about__flex">
+                    <div class="p-about__flex-item">
+                        <div class="p-about__img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/about/about_01.png" alt="">
+                        </div>
+                    </div>
+                    <div class="p-about__flex-item p-about__lead">
+                        <div class="p-about__lead-top">
+                            <h2 class="p-about__title">｢愛がある｡OPERAがある｡｣</h2>
+                        </div>
+                        <div class="p-about__lead-middle">
+                            <p class="p-about__lead-text">今、本当に欲しかった、ニュースタイルのペット葬儀をお⼿元に。<br><span class="u-uppercase">opera</span>は、飼い主様ファーストを⼤切にしています。</p>
+                            <p class="p-about__lead-text">ペットは神様からの最⾼の贈り物。<br>愛で迎えられ、愛に育てられ、愛を残して天へ帰る、儚くも強い命です。<br>「最後だからこそ、丁寧に⾒送りたい」<br>飼い主様のそうしたお気持ちを第⼀に、プランを考えました。</p>
+                            <p class="p-about__lead-text">お別れは、つらく、悲しく、さみしいものですが<br>「ありがとう」を伝えるための、グランドフィナーレでもあります。<br>精⼀杯⽣き抜いた⼩さな家族へ、⾔い尽くせぬほどの感謝を込めて。</p>
+                            <p class="p-about__lead-text">もしもの際はわたくしどもをご⽤命ください。<br>愛あるペット通夜、ペット葬儀を、<span class="u-uppercase">opera</span>からお届けします。</p>
+                        </div>
+                        <div class="p-about__lead-bottom">
+                            <p class="p-about__lead-name">岩井　ゆかり</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="l-commitment p-commitment">
+            <div class="l-container -forSub">
+                <div class="p-commitment__title">
+                    <h2 class="c-title u-centerposition" title="Commitment"><span class="u-uppercase">opera</span>のこだわり</h2>
+                </div>
+                <div class="c-grid p-commitment__box">
+                    <div class="c-grid__item -column2">
+                        <div class="p-commitment__wrap">
+                            <h3 class="p-commitment__lead">小さくて大きな命に、祈りを</h3>
+                            <div class="p-commitment__text">
+                                <p class="c-text">ペットの聖地から来たダニエレをはじめ<br>ミカエル、アンジェロなどの、天国案内⼈が<br>あなたのペットのために深い祈りを捧げます。</p>
+                                <p class="c-text">清らかで無垢な魂は、祈りの⾔葉にみちびかれ<br>安堵と平和をもたらすでしょう。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-grid__item -column2">
+                        <img class="p-commitment__wrap-img" src="<?php echo get_template_directory_uri(); ?>/img/about/about_02.png" alt="">
+                    </div>
+                </div>
+                <hr class="c-border">
+                <div class="c-grid -reverce p-commitment__box">
+                    <div class="c-grid__item -column2">
+                        <div class="p-commitment__wrap -secound">
+                            <h3 class="p-commitment__lead">ご自宅に祭壇を</h3>
+                            <div class="p-commitment__text">
+                                <p class="c-text">献花、祭壇花は代官⼭のフローリスト宇⽥川 優(Aʼs you)が担当<br>ペットを亡くされた飼い主様の⼼にを癒せるように、と<br>かわいらしさと思い出を重ねた作品ができました。<br>ここでしか出会えない美しさをぜひお⼿元に。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-grid__item -column2">
+                        <img class="p-commitment__wrap-img" src="<?php echo get_template_directory_uri(); ?>/img/about/about_03.png" alt="">
+                    </div>
+                </div>
+                <hr class="c-border">
+                <div class="c-grid p-commitment__box">
+                    <div class="c-grid__item -column2">
+                        <div class="p-commitment__wrap">
+                            <h3 class="p-commitment__lead">まるで、白雪姫みたい</h3>
+                            <div class="p-commitment__text">
+                                <p class="c-text">おとぎ話をイメージしたお棺は<br>神戸の専門工房にて制作した特注品です。</p>
+                                <p class="c-text">初めてうちに来た日…初めてのおさんぽ…<br>様々なストーリーを話しかけながら<br>ご遺体周りを生花、<br>パールバルーンをお飾りください<br>美しい装飾で、お弔いの献花をしましょう。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-grid__item -column2">
+                        <img class="p-commitment__wrap-img" src="<?php echo get_template_directory_uri(); ?>/img/about/about_04.png" alt="">
+                    </div>
+                </div>
+                <hr class="c-border">
+            </div>
+        </section>
+        <section class="l-more p-more">
+            <div class="l-container -forSub">
+                <div class="p-more__box">
+                    <div class="p-more__top">
+                        <h2 class="p-more__title"><span class="u-uppercase">opera</span>のサービスをみる</h2>
+                    </div>
+                    <div class="p-more__bottom">
+                        <a href="#" class="p-more__button">自宅葬サービス</a>
+                        <a href="#" class="p-more__button">思い出トランク</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php get_footer(); ?>
