b4cac1461b14f4741fc3d25bc198ac00c2a0dba7
tatsuya-fujikawa <fujikawa@j-cool.fm>
2021-03-22 19:23:06 +0900

    footerリンク繋げた

--- footer.php
+++ footer.php
@@ -9,15 +9,15 @@
             <div class="p-footer__top">
                 <nav class="p-footer__nav">
                     <ul class="p-footer__list">
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;Home &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;OPERAとは &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;自宅葬サービス &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;お焚き上げサービス &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;スタッフ紹介 &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;アフターサポート &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;お知らせ &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;コラム &emsp;/</a></li>
-                        <li class="p-footer__nav-item"><a href="#"> &emsp;プライバシーポリシー &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/")); ?>"> &emsp;Home &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/about/")); ?>"> &emsp;OPERAとは &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/home-funeral/")); ?>"> &emsp;自宅葬サービス &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/service/")); ?>"> &emsp;他サービス &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/omoide/")); ?>"> &emsp;思い出トランク &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/staff/")); ?>"> &emsp;スタッフ紹介 &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/information/")); ?>"> &emsp;お知らせ &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/information#column/")); ?>"> &emsp;コラム &emsp;/</a></li>
+                        <li class="p-footer__nav-item"><a href="<?php echo esc_url( home_url("/contact#privacy/")); ?>"> &emsp;プライバシーポリシー &emsp;/</a></li>
                         <li class="p-footer__nav-item"><a href="#"> &emsp;特定商取引法に基づく表記 &emsp;/</a></li>
                     </ul>
                 </nav>
@@ -30,16 +30,16 @@
                         <p class="p-footer__company-address">東京都港区南青山2丁目2番15号<br>ウィン青山834</p>
                     </div>
                     <div class="p-footer__sns">
-                        <a href="#"><img class="p-footer__sns-instagram" src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="インスタグラムロゴ" width="40px"></a>
-                        <a href="#"><img class="p-footer__sns-line" src="<?php echo get_template_directory_uri(); ?>/img/line.svg" alt="ラインロゴ" width="40px"></a>
+                        <a href="https://www.instagram.com/operaaoyama1122/" traget="_blank" rel="noreferrer noopener"><img class="p-footer__sns-instagram" src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="インスタグラムロゴ" width="40px"></a>
+                        <a href="#" traget="_blank" rel="noreferrer noopener"><img class="p-footer__sns-line" src="<?php echo get_template_directory_uri(); ?>/img/line.svg" alt="ラインロゴ" width="40px"></a>
                     </div>
                 </div>
                 <div class="p-footer__right">
                     <div class="p-footer__right-top">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/mail_btn.png" alt=""></a>
+                        <a href="<?php echo esc_url( home_url("/contact/")); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/mail_btn.png" alt=""></a>
                     </div>
                     <div class="p-footer__right-bottom">
-                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/freedial.png" alt=""></a>
+                        <a href="tel:0120-71-1212"><img src="<?php echo get_template_directory_uri(); ?>/img/freedial.png" alt=""></a>
                     </div>
                 </div>
             </div>
@@ -53,14 +53,14 @@
         <div class="p-footer__sp sp">
             <div class="p-footer__sp">
                 <div class="p-footer__sp-tel">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/sp/footer_freedial.png" alt=""></a>
+                    <a href="tel:0120-71-1212"><img src="<?php echo get_template_directory_uri(); ?>/img/sp/footer_freedial.png" alt=""></a>
                 </div>
                 <div class="p-footer__sp-mail">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/sp/footer_mail.png" alt=""></a>
+                    <a href="<?php echo esc_url( home_url("/contact/")); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/sp/footer_mail.png" alt=""></a>
                     <p class="p-footer__sp-text">※生前のご相談も承っております</p>
                 </div>
                 <div class="p-footer__sns">
-                    <a href="#"><img class="p-footer__sns-instagram" src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="インスタグラムロゴ" width="60px"></a>
+                    <a href="https://www.instagram.com/operaaoyama1122/"><img class="p-footer__sns-instagram" src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="インスタグラムロゴ" width="60px" traget="_blank" rel="noreferrer noopener"></a>
                     <a href="#"><img class="p-footer__sns-line" src="<?php echo get_template_directory_uri(); ?>/img/line.svg" alt="ラインロゴ" width="60px"></a>
                 </div>
                 <div class="p-footer__company-sp">
@@ -71,15 +71,15 @@
             <div class="p-footer__sp-list">
                 <nav class="p-footer__sp-nav">
                     <ul class="p-footer__sp-list">
-                        <li class="p-footer__sp-list-item"><a href="#"><span class="uppercase">Home</span></a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">自宅葬サービス</a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">お知らせ</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/")); ?>"><span class="uppercase">Home</span></a></li>
+                        <li class="p-footer__sp-list-item"><a href=<?php echo esc_url( home_url("/home-funeral/")); ?>">自宅葬サービス</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/information/")); ?>">お知らせ</a></li>
                         <li class="p-footer__sp-list-item"><a href="#">火葬サービス</a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">コラム</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/information#column/")); ?>">コラム</a></li>
                         <li class="p-footer__sp-list-item"><a href="#">メモリアルサービス</a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">お問い合わせ</a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">スタッフ紹介</a></li>
-                        <li class="p-footer__sp-list-item"><a href="#">プライバシーポリシー</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/contact/")); ?>">お問い合わせ</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/staff/")); ?>">スタッフ紹介</a></li>
+                        <li class="p-footer__sp-list-item"><a href="<?php echo esc_url( home_url("/contact#privacy/")); ?>">プライバシーポリシー</a></li>
                         <li class="p-footer__sp-list-item"><a href="#">ペットロスサポート</a></li>
                         <li class="p-footer__sp-list-item"><a href="#">特定商取引法に基づく表記</a></li>
                     </ul>
@@ -93,7 +93,7 @@
     <div class="l-bottom p-bottom sp">
         <div class="p-bottom__wrapper">
             <div class="p-bottom__left">
-                <a href="">
+                <a href="tel:0120711212">
                     <div class="p-bottom__flex">
                         <div class="p-bottom__icon">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/sp/call.png" alt="">
@@ -105,7 +105,7 @@
                 </a>
             </div>
             <div class="p-bottom__right">
-                <a href="">
+                <a href="<?php echo esc_url( home_url("/contact/")); ?>">
                     <div class="p-bottom__flex">
                         <div class="p-bottom__icon -mail">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/sp/mail.png" alt="">
