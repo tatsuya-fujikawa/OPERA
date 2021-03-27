56c99cddd4d4937013e932e086dbf4fa1460089d
fujikawa <kyuji123456@yahoo.co.jp>
2021-03-28 02:27:21 +0900

    no message

---
+++ page-service.php
@@ -0,0 +1,142 @@
+<?php
+/*
+Template Name: 他サービス
+*/
+?>
+<?php get_header("2"); ?>
+    <main>
+        <section class="l-spacer -nobottom">
+            <div class="c-subtitle -other">
+                <h2 class="c-title02 u-centerposition" title="Service">他サービス</h2>
+            </div>
+        </section>
+        <section class="l-staff p-staff p-other">
+            <div class="l-container -forSub">
+                <div class="p-staff__wrapper">
+                    <h2 class="c-heading" title="consultation">生前のご相談</h2>
+                    <div class="p-staff__flex p-other__flex">
+                        <div class="p-staff__flex-item -column2 p-staff__left">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/service/service_01.png" alt="ご相談員">
+                        </div>
+                        <div class="p-staff__flex-item -column2">
+                            <div class="p-other__wrap">
+                                <h3 class="p-other__title">お話を伺い、モヤモヤをスッキリさせます</h3>
+                                <p class="p-other__text -lead">相談員がオンラインでお話を伺います。</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">料⾦</p>
+                                <p class="p-other__text">初回<span class="p-other__text-strong">￥0</span>（税込）</p>
+                                <p class="p-other__text">2回⽬から<span class="p-other__text-strong">￥3,000</span>（税込）/30分</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">ご相談⽅法</p>
+                                <p class="p-other__text">ZOOM、LINE電話、電話</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">お⽀払い⽅法</p>
+                                <p class="p-other__text">カード決済⼊⾦、銀⾏振込</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="c-border">
+                <div class="p-staff__wrapper">
+                    <h3 class="c-heading" title="aroma grooming">アロマグルーミング</h3>
+                    <div class="p-staff__flex p-other__flex">
+                        <div class="p-staff__flex-item -column2 p-staff__left">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/service/service_02.png" alt="トリミング中">
+                        </div>
+                        <div class="p-staff__flex-item -column2">
+                            <div class="p-other__wrap">
+                                <h3 class="p-other__title">つらい治療で疲れたお⾝体を、<br>真⼼込めてきれいにします</h3>
+                                <p class="p-other__text -lead">大切なペットの身だしなみを整えます。<br>トリマーがご自宅までお伺いします。</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">料⾦</p>
+                                <p class="p-other__text"><span class="p-other__text-strong">￥10,500</span>（税込）</p>
+                                <p class="p-other__text">トリマー1名派遣/部分湯灌/アロマコートブラシ/お⽖切り/顔回りカット/部分カット/アイシング等/死後処置</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">対象エリア</p>
+                                <p class="p-other__text">東京都（23区）・神奈川県（横浜市・川崎市）</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">お⽀払い⽅法</p>
+                                <p class="p-other__text">カード決済（当日お支払い）、銀行振込</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="c-border">
+                <div class="p-staff__wrapper">
+                    <h3 class="c-heading" title="powder addition">パウダー加工</h3>
+                    <div class="p-staff__flex">
+                        <div class="p-staff__flex-item -column2 p-staff__left">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/service/service_03.png" alt="パウダー加工">
+                        </div>
+                        <div class="p-staff__flex-item -column2">
+                            <div class="p-other__wrap">
+                                <h3 class="p-other__title">お骨を粉状にすることで、<br>散骨や樹木葬、お手元供養等に</h3>
+                                <p class="p-other__text -lead">お⾻上げのあとそのまま⽕葬スタッフにお預けください。<br>パウダー加⼯を施し、翌⽉命⽇までにご家庭へお戻しいたします。<br>（後⽇でも承りますのでお気軽にご相談ください）</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">料⾦</p>
+                                <p class="p-other__text"><span class="p-other__text-strong">￥21,000</span>（税込）※お⾻お帰し料込</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">対象エリア</p>
+                                <p class="p-other__text">東京都（23区）・神奈川県（横浜市・川崎市）</p>
+                            </div>
+                            <div class="p-other__wrap">
+                                <p class="c-subheading">お⽀払い⽅法</p>
+                                <p class="p-other__text">カード決済入金、銀行振込、カード決済（当日お支払い）</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="c-border">
+            </div>
+        </section>
+        <section class="l-instructions p-instrunctions">
+            <div class="l-container -forSub">
+                <div class="l-spacer">
+                    <h2 class="c-heading" title="payment">お支払い方法</h2>
+                </div>
+                <div class="p-instrunctions__inner">
+                    <div class="l-spacer -medium -notop">
+                        <p class="p-instrunctions__text">「他サービス」のお⽀払い⽅法です。<br>承る時期やサービスの併⽤などで、お⽀払い⽅法が変更となることもございます。詳しくはお問い合わせください。</p>
+                    </div>
+                    <div class="p-instrunctions__box">
+                        <h3 class="c-subheading">カード決済入金</h3>
+                        <p class="p-instrunctions__text-inline -smt">代⾦先払い(振込⼿数料：お客様手数料０円)</p>
+                        <p class="p-instrunctions__text">Square (スクエア)を利⽤したクレジットカード⽀払いが可能です。<br>お問い合わせいただきましてから、決済ページのご案内をお送りします。</p>
+                    </div>
+                    <div class="p-instrunctions__box">
+                        <h3 class="c-subheading">銀行振込</h3>
+                        <p class="p-instrunctions__text-inline -smt">代⾦先払い(振込⼿数料：お客様ご負担)</p>
+                        <ul class="p-instrunctions__list">
+                            <li>・お振込みのご名義は、ご注⽂者様のお名前にてお願いします。</li>
+                            <li>・ご注⽂商品のお⽀払⾦額に対して、振込のご⼊⾦額が多く振り込まれた場合は、振込⼿数料を差し引いてご返⾦いたします。</li>
+                            <li>※振込⼿数料を差し引いた⾦額がマイナスとなる場合は返⾦いたしかねます。</li>
+                        </ul>
+                        <ul class="p-instrunctions__list">
+                            <li>銀行振込・お振込先</li>
+                            <li>⾦融機関名：みずほ銀⾏（0001）</li>
+                            <li>⽀店名：⻘⼭⽀店（211）</li>
+                            <li>科⽬：普通 ⼝座番号：2621515</li>
+                            <li>受取⼈名：カ）グツドラツクアンドデイー</li>
+                        </ul>
+                    </div>
+                    <div class="p-instrunctions__box">
+                        <h3 class="c-subheading">カード決済</h3>
+                        <p class="p-instrunctions__text-inline -smt">当⽇お⽀払い(お客様⼿数料0円)</p>
+                        <p class="p-instrunctions__text">クレジット決済端末を利⽤したカード⽀払いが可能です。</p>
+                    </div>
+                </div>
+                <div class="l-spacer -section -notop">
+                    <?php get_template_part('inc/cta'); ?>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php get_footer(); ?>
